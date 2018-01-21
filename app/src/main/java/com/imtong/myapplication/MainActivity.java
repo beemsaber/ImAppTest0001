68669b5e5812b412fd4e455d653feb83ab9df9e2
ImTong <beemsaber@hotmail.com>
2018-01-21 14:58:36 +0700

    test system 222

--- app/src/main/java/com/imtong/myapplication/MainActivity.java
+++ app/src/main/java/com/imtong/myapplication/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //45322
-        //assasdf
+        //assasdf  mmhft
     }
 }
